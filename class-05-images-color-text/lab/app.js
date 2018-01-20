8092594cb90aaaadd68f0bee9a8746dddb0ca01a
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-01-20 11:30:40 -0800

    first question passed

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -9,11 +9,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
+  var output = a + b;
+  var string = 'The sum of ' + a + ' and ' + b + ' is ' + output + '.';
+  var answer = [output, string];
+  return answer;
   //eslint-disable-line
 }
-
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
+
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
